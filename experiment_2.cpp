89a550a69c6d9a5e8510d2d889fd4738a50efddb
utkarsh chaudhary <chaudryutkarsh@gmail.com>
2024-07-29 22:37:18 +0530

    Create experiment_2.cpp

---
+++ experiment_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+     int a1;
+     float a2;
+      char a3;
+      short int a4;
+      long int a5;
+      double a6;
+    
+
+    std::cout << "Size of a1: " << sizeof(a1) << "byte"<< std::endl;
+    std::cout << "Size of a2: " << sizeof(a2) << "byte"<< std::endl;
+    std::cout << "Size of a3: " << sizeof(a3) << "byte"<< std::endl;
+    std::cout << "Size of a4: " << sizeof(a4) << "byte"<< std::endl;
+    std::cout << "Size of a5: " << sizeof(a5) << "byte"<< std::endl;
+    std::cout << "Size of a6: " << sizeof(a6) << "byte"<< std::endl;
+    
+    return 0;
+}
